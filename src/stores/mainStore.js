bd75bca697e71d00feb03de75c96c6c8ba06f18c
Hannes Vaupel - Desktop <hvaupel@kabelmail.de>
2020-01-30 21:10:43 +0100

    Updated Check E-Mail RegEx

--- src/stores/mainStore.js
+++ src/stores/mainStore.js
@@ -188,8 +188,8 @@ class MainStore {
             return
         }
 
-        //Prüfung ob E-Mail korrekte Adresse ist
-        if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(this.registerEmail)) {
+        //Prüfung ob E-Mail korrekte Adresse ist. Die RegEx prüft die E-Mail auf Korrektheit.
+        if (!/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(this.registerEmail)) {
             this.errorMsg = 'Bitte korrekte E-Mail Adresse angeben.'
             return
         }
