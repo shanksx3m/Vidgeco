b88467ede98ed99f69a3e6afb215e853571cb966
Hannes Vaupel - Desktop <hvaupel@kabelmail.de>
2020-01-30 22:34:22 +0100

    produkte anzeigen

--- src/components/Household.jsx
+++ src/components/Household.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { observer, inject } from 'mobx-react';
 import Product from './Product';
 import mainStore from '../stores/mainStore';
+import Octicon, { Trashcan, Pencil } from '@primer/octicons-react'; //Octicon ist eine Sammlung an Icons (https://octicons.github.com/)
 
 @inject('mainStore')
 @observer
@@ -9,7 +10,7 @@ import mainStore from '../stores/mainStore';
 //Zeigt übersicht über alle Produkte/Zutaten mit allen Daten an. Löschung der Daten durch Buttonclick möglich. 
 //Die Ausgabe erfolgt dir die Class 'Product' aus der Produkt.jsx
 class Household extends React.Component {
-    render() {   
+    render() {
         const { householdName, products } = this.props.mainStore;
 
         return (
@@ -18,11 +19,26 @@ class Household extends React.Component {
                     {/* Name des Haushaltes */}
                     <p className="h1">{householdName}</p>
                     <p className="h4"></p>
-                    
-                    {/* Anzeige der Produkte mit Hilfe der Klasse 'Product' aus der Datei Product.jsx */}
+
                     <div className="Produkte" className="container">
-                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4">                        
-                            <div className="col product-div"><Product /></div>
+                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+                            {products.map(product => {
+                                <div className='product-output'>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            <img src={product.image} className="card-img" alt="" />
+                                            {product.name}
+                                        </h5>
+                
+                                        <h6 className="card-subtitle mb-2 text-muted">{product.menge + ' ' + product.einheit} im {product.lagerort}<br />Haltbar bis {product.mhd}</h6>
+                                        <button className="card-link btn btn-dark"><Octicon icon={Pencil} /></button>
+                
+                                        <button className="card-link btn btn-danger"><Octicon icon={Trashcan} /></button></div>
+                                </div>
+                            </div>
+                            })}
+                            {/* <div className="col product-div"><Product /></div> */}
                             {/* <div className="col product-div"><Product id="1" name='Eier' menge='10 Stück' mhd='27.01.2020' lagerort='Eckschrank' image="https://cdn.pixabay.com/photo/2018/02/26/16/30/eggs-3183410_960_720.jpg" /></div>
                             <div className="col product-div"><Product id="2" name='Schweine-Hack' menge='200 g' mhd='13.01.2020' lagerort='Kühlschrank' image="https://cdn.pixabay.com/photo/2020/01/09/13/16/mett-4752771_960_720.jpg" /></div>
                             <div className="col product-div"><Product id="3" name='Gouda' menge='1 Stück' mhd='20.01.2020' lagerort='Kühlschrank' image="https://cdn.pixabay.com/photo/2013/02/13/17/44/cheese-81403_960_720.jpg" /></div>
